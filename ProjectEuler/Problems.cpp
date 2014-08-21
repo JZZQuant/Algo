37f4ea767f230c67d0f86962a2ef1794305cfd20
Sasikanth.Raghava <Sasikanth.Raghava@INDEVWKS13.1e.local>
2014-08-21 13:42:50 +0530

    Hello world Commit

---
+++ ProjectEuler/Problems.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Problems.cpp
+// Author      : Sasikanth
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+
+int main(void) {
+	cout << "Data Structure Algorithms";
+}
