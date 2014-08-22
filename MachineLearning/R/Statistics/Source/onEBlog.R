6b2eeadd3ddf637ffec6be290b5675fa9a314989
Sasikanth.Raghava <Sasikanth.Raghava@INDEVWKS13.1e.local>
2014-08-22 11:36:34 +0530

    Remove Stats

--- MachineLearning/R/Statistics/Source/onEBlog.R
+++
@@ -1,8 +0,0 @@
-library(RJSONIO)
-
-#Loading Data in R --------------------------------------------------------------------------------------------------
-
-setwd("C:\\Users\\Sasikanth.Raghava\\workspace\\OneEBlog\\ScrappedData")
-blogs<-list.files(pattern="\\.json")
-bloglists<-lapply(blogs,fromJSON)
-allblogs<-unlist(bloglists,recursive=F)
