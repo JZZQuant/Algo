0f04d757ddf558cccc0ba3dc7ccef5b15632e6ef
JZQuant <v00d00childblues1@gmail.com>
2014-11-24 20:22:50 +0530

    Final solution for finding nearest blogs

--- MachineLearning/R/Statistics/Src/onEBlog.R
+++ MachineLearning/R/Statistics/Src/onEBlog.R
@@ -2,12 +2,13 @@ library(RJSONIO)
 library(wordcloud)
 library(ggplot2)
 #Loading Data in R --------------------------------------------------------------------------------------------------
-
-setwd("C:/Users/Sasikanth.Raghava/Documents/git/bitbucket/algorithms/MachineLearning/Python/OneEBlog/Scrapped")
+setwd("C:/Users/Raghava/Documents/git/bitbucket/algorithms/MachineLearning/Python/OneEBlog/Scrapped")
+#load all the json files and list them into a list
 blogs<-list.files(pattern="\\.json")
 bloglists<-lapply(blogs,fromJSON)
 allblogs<-unlist(bloglists,recursive=F)
 names(allblogs[[1]])
+#get the content to a json listand comment respone
 comments.count<- as.numeric(sapply(allblogs,function(x) {if(length(x[[4]])==1 && x[[4]]=="" ) 0 else length(x[[4]])}))
 content<- as.character( sapply(allblogs,function(x) paste(x[[5]],collapse=" ")))
 
@@ -26,29 +27,45 @@ head(sort(table(as.factor(unlist(sapply(allblogs, function(x){unlist(x[['Author'
 #ggplot2 and nlp of tm have conflicting issues
 detach("package:ggplot2", unload=TRUE)
 
+# create a corpus and clean it up
 library(tm)
-blogcorp<-Corpus(VectorSource(df$content))
+blogcorp<-Corpus(VectorSource(content))
 clean<-tm_map(blogcorp,tolower)
 clean<-tm_map(blogcorp,removeNumbers)
 clean<-tm_map(clean,removeWords,stopwords())
 clean<-tm_map(clean,removePunctuation)
 clean<-tm_map(clean,stripWhitespace)
 clean <- tm_map(clean, PlainTextDocument)
-wordcloud(clean, min.freq = 40, random.order = FALSE)
+#wordcloud of  most repeated words and tags
+wordcloud(clean, min.freq = 40, random.order = FALSE,colors=brewer.pal(8, "Dark2"))
+
+#create a document term matrix and remove too obvious and unwanted words
 dtm<-DocumentTermMatrix(clean)
-#dtm<-removeSparseTerms(dtm, 0.97)
 onedict<-findFreqTerms(dtm,lowfreq=1, highfreq=10)
 onetrain <- DocumentTermMatrix(clean,list(dictionary = onedict))
-#convert_counts <- function(x) {x <- ifelse(x > 0, 1, 0); x <- factor(x, levels = c(0, 1), labels = c("No", "Yes")); return(x)}
+
 # convert to a markhov stochaistic matrix
-convert_counts <- function(x) {x <- ifelse(x > 0, 1, 0); return(x)}
-b.m <- apply(onetrain, MARGIN = 2, convert_counts)
-m.m<-t(t(b.m)/colSums(b.m))
+bayesian.counts <- function(x) {x <- ifelse(x > 0, 1, 0); return(x)}
+# a logical matrix to map all words to documents inverted indices
+boolean.matrix <- apply(onetrain, MARGIN = 2, bayesian.counts)
+# divide by column sum to convert the matrix into a markov like stohaistic matrix
+stochaitic.matrix<-t(t(boolean.matrix)/colSums(boolean.matrix))
 
-test<-11
-#diiferential
-d<-b.m-b.m[test,]
-#normalize
-b.m <- apply(b.m, MARGIN = 2, convert_counts)
-#probabilistic scalling
+# unsupervised learning algorithm to find similar duplicates
+find.duplicates <-function(test)
+{
+    #diiferential
+    linear.difference<-t(t(boolean.matrix)-boolean.matrix[test,])
+    #normalize
+    normal.counts <- function(x) {x <- ifelse(x == 0, 0, 1); return(x)}
+    normal.matrix <- apply(linear.difference, MARGIN = 2, normal.counts)
+    #scalling
+    linear.span<-t(t(normal.matrix)*stochaitic.matrix[test,])
+    #find the most similar looking blogs
+    nearestblogs<-unique(match(head(sort(rowSums(linear.span)),100),rowSums(linear.span)))
+    #get the top two neighbours
+    sapply(allblogs[nearestblogs], function(x){unlist(x[['URL']])})[1:2]
+}
 
+# find duplicates
+lapply(c(143,56,37,745),find.duplicates)
